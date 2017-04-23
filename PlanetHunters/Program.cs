b8e14d2c90d1a91e4cc81734bc14d66f28d73f77
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-04-23 20:03:54 +0300

    upload

--- PlanetHunters/Program.cs
+++ PlanetHunters/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using Newtonsoft.Json;
+using PlanetHunters.Import.JSON;
+using System.IO;
+using PlanetHunters.Import.DTO;
 
 namespace PlanetHunters
 {
@@ -12,7 +16,7 @@ namespace PlanetHunters
         static void Main(string[] args)
         {
             var context = new PlanetHuntersContext();
-            
+            ImportTelescopes(context);
            
         }
         public static void InitDB(PlanetHuntersContext cnxt)
@@ -29,6 +33,24 @@ namespace PlanetHunters
                 Thread.Sleep(30);
             }
             Console.WriteLine();
+        }
+        public static void ImportAstronomers(PlanetHuntersContext context)
+        {
+           PhaseTyper("Reading file Astronomers.json");
+            var json = File.ReadAllText(@"C:\Users\Mihail\Documents\visual studio 2015\Projects\PlanetHunters\PlanetHunters\Import\JSON\astronomers.json");
+
+            var astronomersDTOsList = JsonConvert.DeserializeObject<IEnumerable<AstronomerDTO>>(json);
+            ImportJSON.AddAstronomers(astronomersDTOsList);
+            
+            
+        }
+        public static void ImportTelescopes(PlanetHuntersContext context)
+        {
+            PhaseTyper("Reading telescopes.json");
+            var json = File.ReadAllText(@"C:\Users\Mihail\Documents\visual studio 2015\Projects\PlanetHunters\PlanetHunters\Import\JSON\telescopes.json");
+            var List = JsonConvert.DeserializeObject<IEnumerable<TelescopeDTO>>(json);
+            ImportJSON.AddTelescopes(List);
+            PhaseTyper("Telescopes added to database succesfully");
         }
     }
 }
